fadeba28d1e2509ba1d3848473351f1c73270b69
LeandroMassola <leandromassola.lm@gmail.com>
2024-11-15 16:35:33 +0100

    cambie router a app.js

--- src/main.jsx
+++ src/main.jsx
@@ -9,9 +9,9 @@ import { BrowserRouter as Router } from 'react-router-dom'
 createRoot(document.getElementById('root')).render(
   
     
-      <Router>
+      
         <App />
-      </Router>
+      
     
   
   ,
